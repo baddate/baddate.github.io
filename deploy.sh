3db68fd98b4d3be7b53dc52540bc1c0eb072543d
alex <badgatex@outlook.com>
2020-07-05 14:14:56 +0800

    modified: summaryShape & sh script

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,6 @@ echo -e "\033[0;32mPulling updates to Local...\033[0m"
 git pull
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-# cd public
 # Add changes to git.
 git add .
 
